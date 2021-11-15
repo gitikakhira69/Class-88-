bb78164833e318d3b2fc84c21324f72245563e73
[gitika] <[gitikakhira69]>
2021-11-15 20:59:56 +0530

    C88

---
+++ booster.js
@@ -0,0 +1,29 @@
+class Booster{
+    constructor(x, y, r) {
+        var options = {
+          restitution: 0.5,
+          frictionAir :0.1,
+          density: 1
+        }
+        x += random(-300, 600);
+        this.body = Bodies.circle(x, y, r, options);
+        this.r = r;
+        this.lifetime = 500;
+        this.body.label = "booster";
+    
+        World.add(world, this.body);
+      }
+      
+     
+      
+      display() {
+          
+        fill("yellow");
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        ellipseMode(RADIUS)
+        ellipse(0, 0, this.r);
+        pop();
+      }
+    }
--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -8,6 +10,7 @@ var particles=[];
 var player1,ground;
 var playerLife = 0;
 var backgroundImg;
+var b_partical = [];
 function preload() {
   backgroundImg = loadImage("images/bg1.jpg");
   
@@ -39,6 +42,7 @@ function setup() {
     if (frameCount % 30 == 0) {
         newParticle();
       }
+      
       for (var i = 0; i < particles.length; i++) {
         particles[i].display();
         if(particles[i].body.position.y>370){
@@ -48,11 +52,38 @@ function setup() {
             
         }
       }
+      if(frameCount % 100 === 0){
+        boosterPartical()
+      }
+      for(var i = 0 ;i < b_partical.length;i++){
+        b_partical[i].display();
+        if(b_partical[i].body.position.y>365){
+          World.remove(world,b_partical[i].body);
+          b_partical.splice(i,1);
+          i--;
+        }
+      }
     ground.display();
     player1.display();
+    Events.on(engine,"collisionStart",collision)
+  }
+  function collision(event){
+    console.log(event.pairs[0])
+    var pair = event.pairs
+    for(var i; i < pair.length;i++){
+      var bodyA = pair[i].bodyA.label
+      var bodyB = pair[i].bodyB.label
+      if(bodyA == "particlas" && bodyB == "player" || bodyA == "player" && bodyB == "partical"){
+        Matter.Body.setPosition(player1.body,{x:mouseX,y:player1.body.position.y+0.05})
+      }
+    }
+  }
+  function boosterPartical(){
+    var p = new Booster(random(10,width-10), 0, random(5, 10));
+      b_partical.push(p);
   }
     function newParticle() {
-      var p = new Particle(600, 0, random(5, 10));
+      var p = new Particle(random(10,width-10), 0, random(5, 10));
       particles.push(p);
     }
   
